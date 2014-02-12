0502c3ca6de4676612ca468baa5f98be401e5179
mamay@notan.ua <rfHfek12>
2014-02-12 16:23:53 +0200

    add BHD and SAR

--- Numbers/Words/lang.en_US.php
+++ Numbers/Words/lang.en_US.php
@@ -232,8 +232,10 @@ class Numbers_Words_en_US extends Numbers_Words
     var $_currency_names = array(
       'AED' => array(array('Dirham'), array('fil')),
       'DIR' => array(array('Dirham'), array('fil')),
+       #mamay
       'BHD' => array(array('Dinar'), array('fil')),
       'SAR' => array(array('Riyal'), array('halala')),
+
       'ALL' => array(array('lek'), array('qindarka')),
       'AUD' => array(array('Australian dollar'), array('cent')),
       'BAM' => array(array('convertible marka'), array('fenig')),
