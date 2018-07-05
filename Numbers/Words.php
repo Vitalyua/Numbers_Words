fc986aa71b661d5168998dbac866def80553ceeb
mamay@notan.ua <rfHfek12>
2018-07-05 11:43:45 +0300

    add ru autoload

--- Numbers/Words.php
+++ Numbers/Words.php
@@ -2,14 +2,14 @@
 /**
  * Numbers_Words
  *
- * PHP version 4
+ * PHP version 5
  *
  * Copyright (c) 1997-2006 The PHP Group
  *
- * This source file is subject to version 3.0 of the PHP license,
+ * This source file is subject to version 3.01 of the PHP license,
  * that is bundled with this package in the file LICENSE, and is
  * available at through the world-wide-web at
- * http://www.php.net/license/3_0.txt.
+ * http://www.php.net/license/3_01.txt
  * If you did not receive a copy of the PHP license and are unable to
  * obtain it through the world-wide-web, please send a note to
  * license@php.net so we can mail you a copy immediately.
@@ -19,12 +19,13 @@
  * @category Numbers
  * @package  Numbers_Words
  * @author   Piotr Klaban <makler@man.torun.pl>
- * @license  PHP 3.0 http://www.php.net/license/3_0.txt
- * @version  CVS: $Id: Words.php 295090 2010-02-15 06:38:34Z clockwerx $
+ * @license  PHP 3.01 http://www.php.net/license/3_01.txt
+ * @version  SVN: $Id$
  * @link     http://pear.php.net/package/Numbers_Words
  */
 
 // {{{ Numbers_Words
+require_once 'Numbers/Words/Exception.php';
 
 /**
  * The Numbers_Words class provides method to convert arabic numerals to words.
@@ -32,52 +33,101 @@
  * @category Numbers
  * @package  Numbers_Words
  * @author   Piotr Klaban <makler@man.torun.pl>
- * @license  PHP 3.0 http://www.php.net/license/3_0.txt
+ * @license  PHP 3.01 http://www.php.net/license/3_01.txt
  * @link     http://pear.php.net/package/Numbers_Words
  * @since    PHP 4.2.3
  * @access   public
  */
 class Numbers_Words
 {
+    // {{{ constants
+
+    /**
+     * Masculine gender, for languages that need it
+     */
+    const GENDER_MASCULINE = 0;
+
+    /**
+      * Feminine gender, for languages that need it
+      */
+    const GENDER_FEMININE = 1;
+
+    /**
+      * Neuter gender, for languages that need it
+      */
+    const GENDER_NEUTER = 2;
+
+    /**
+      * This is not an actual gender; some languages
+      * have different ways of numbering actual things
+      * (e.g. Romanian: "un nor, doi nori" for "one cloud, two clouds")
+      * and for just counting in an abstract manner
+      * (e.g. Romanian: "unu, doi" for "one, two"
+      */
+    const GENDER_ABSTRACT = 3;
+
+    // }}}
+
+    // {{{ properties
+
+    /**
+     * Default Locale name
+     * @var string
+     * @access public
+     */
+    public $locale = 'en_US';
+
+    /**
+     * Default decimal mark
+     * @var string
+     * @access public
+     */
+    public $decimalPoint = '.';
+
+    // }}}
     // {{{ toWords()
 
     /**
      * Converts a number to its word representation
      *
-     * @param integer $num    An integer between -infinity and infinity inclusive :)
-     *                        that should be converted to a words representation
-     * @param string  $locale Language name abbreviation. Optional. Defaults to en_US.
+     * @param integer $num     An integer between -infinity and infinity inclusive :)
+     *                         that should be converted to a words representation
+     * @param string  $locale  Language name abbreviation. Optional. Defaults to
+     *                         current loaded driver or en_US if any.
+     * @param array   $options Specific driver options
      *
      * @access public
      * @author Piotr Klaban <makler@man.torun.pl>
      * @since  PHP 4.2.3
      * @return string  The corresponding word representation
      */
-    function toWords($num, $locale = 'en_US')
+    function toWords($num, $locale = '', $options = array())
     {
-
-        include_once "Numbers/Words/lang.${locale}.php";
-
-        $classname = "Numbers_Words_${locale}";
-
-        if (!class_exists($classname)) {
-            return Numbers_Words::raiseError("Unable to include the Numbers/Words/lang.${locale}.php file");
+        if (empty($locale) && isset($this) && $this instanceof Numbers_Words) {
+            $locale = $this->locale;
         }
 
-        $methods = get_class_methods($classname);
-
-        if (!in_array('toWords', $methods) && !in_array('towords', $methods)) {
-            return Numbers_Words::raiseError("Unable to find toWords method in '$classname' class");
+        if (empty($locale)) {
+            $locale = 'en_US';
         }
 
-        @$obj = new $classname;
+        $classname = self::loadLocale($locale, '_toWords');
+
+
+        $obj = new $classname;
+
 
         if (!is_int($num)) {
+            $num = $obj->normalizeNumber($num);
+
             // cast (sanitize) to int without losing precision
-            $num = preg_replace('/^[^\d]*?(-?)[ \t\n]*?(\d+)([^\d].*?)?$/', '$1$2', $num);
+            $num = preg_replace('/(.*?)('.preg_quote($obj->decimalPoint).'.*?)?$/', '$1', $num);
         }
 
-        return trim($obj->toWords($num));
+        if (empty($options)) {
+            return trim($obj->_toWords($num));
+        }
+        return trim($obj->_toWords($num, $options));
     }
     // }}}
 
@@ -90,10 +140,14 @@ class Numbers_Words
      *
      * @param string $locale   Language name abbreviation. Optional. Defaults to en_US.
      *
-     * @param string $int_curr International currency symbol
-     *                 as defined by the ISO 4217 standard (three characters).
-     *                 E.g. 'EUR', 'USD', 'PLN'. Optional.
-     *                 Defaults to $def_currency defined in the language class.
+     * @param string $intCurr  International currency symbol
+     *                         as defined by the ISO 4217 standard (three characters).
+     *                         E.g. 'EUR', 'USD', 'PLN'. Optional.
+     *                         Defaults to $def_currency defined in the language class.
+     *
+     * @param string $decimalPoint  Decimal mark symbol
+     *                         E.g. '.', ','. Optional.
+     *                         Defaults to $decimalPoint defined in the language class.
      *
      * @return string  The corresponding word representation
      *
@@ -102,36 +156,30 @@ class Numbers_Words
      * @since  PHP 4.2.3
      * @return string
      */
-    function toCurrency($num, $locale = 'en_US', $int_curr = '')
+    function toCurrency($num, $locale = 'en_US', $intCurr = '', $decimalPoint = null)
     {
         $ret = $num;
 
-        include_once "Numbers/Words/lang.${locale}.php";
+        $classname = self::loadLocale($locale, 'toCurrencyWords');
 
-        $classname = "Numbers_Words_${locale}";
+        $obj = new $classname;
 
-        if (!class_exists($classname)) {
-            return Numbers_Words::raiseError("Unable to include the Numbers/Words/lang.${locale}.php file");
+        if (is_null($decimalPoint)) {
+            $decimalPoint = $obj->decimalPoint;
         }
-
-        $methods = get_class_methods($classname);
-
-        if (!in_array('toCurrencyWords', $methods) && !in_array('tocurrencywords', $methods)) {
-            return Numbers_Words::raiseError("Unable to find toCurrencyWords method in '$classname' class");
-        }
-
-        @$obj = new $classname;
 
         // round if a float is passed, use Math_BigInteger otherwise
         if (is_float($num)) {
             $num = round($num, 2);
         }
 
-        if (strpos($num, '.') === false) {
-            return trim($obj->toCurrencyWords($int_curr, $num));
+        $num = $obj->normalizeNumber($num, $decimalPoint);
+
+        if (strpos($num, $decimalPoint) === false) {
+            return trim($obj->toCurrencyWords($intCurr, $num));
         }
 
-        $currency = explode('.', $num, 2);
+        $currency = explode($decimalPoint, $num, 2);
 
         $len = strlen($currency[1]);
 
@@ -147,10 +195,10 @@ class Numbers_Words
             
             if ($round_digit >= 5) {
                 // round up without losing precision
-                //include_once "Math/BigInteger.php";
+                include_once "Math/BigInteger.php";
 
-                $int = new \phpseclib\Math\BigInteger(join($currency));
-                $int = $int->add(new \phpseclib\Math\BigInteger(1));
+                $int = new Math_BigInteger(join($currency));
+                $int = $int->add(new Math_BigInteger(1));
                 $int_str = $int->toString();
 
                 $currency[0] = substr($int_str, 0, -2);
@@ -163,7 +211,7 @@ class Numbers_Words
             }
         }
 
-        return trim($obj->toCurrencyWords($int_curr, $currency[0], $currency[1]));
+        return trim($obj->toCurrencyWords($intCurr, $currency[0], $currency[1]));
     }
     // }}}
 
@@ -171,64 +219,105 @@ class Numbers_Words
     /**
      * Lists available locales for Numbers_Words
      *
-     * @param mixed $locale string/array of strings $locale
-     *                 Optional searched language name abbreviation.
-     *                 Default: all available locales.
+     * @param mixed $locales string/array of strings $locale
+     *                       Optional searched language name abbreviation.
+     *                       Default: all available locales.
      *
      * @return array   The available locales (optionaly only the requested ones)
      * @author Piotr Klaban <makler@man.torun.pl>
      * @author Bertrand Gugger, bertrand at toggg dot com
      *
-     * @access public
-     * @static
-     * @return mixed[]
+     * @return mixed[] Array of locale names ("de_DE", "en")
      */
-    function getLocales($locale = null)
+    public static function getLocales($locales = null)
     {
         $ret = array();
-        if (isset($locale) && is_string($locale)) {
-            $locale = array($locale);
+        if (isset($locales) && is_string($locales)) {
+            $locales = array($locales);
         }
 
-        $dname = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Words' . DIRECTORY_SEPARATOR;
+        $dname = __DIR__ . DIRECTORY_SEPARATOR . 'Words'
+            . DIRECTORY_SEPARATOR . 'Locale'
+            . DIRECTORY_SEPARATOR;
 
-        $dh = opendir($dname);
-
-        if ($dh) {
-            while ($fname = readdir($dh)) {
-                if (preg_match('#^lang\.([a-z_]+)\.php$#i', $fname, $matches)) {
-                    if (is_file($dname . $fname) && is_readable($dname . $fname) &&
-                        (!isset($locale) || in_array($matches[1], $locale))) {
-                        $ret[] = $matches[1];
-                    }
-                }
+        $sfiles = glob($dname . '??.php');
+        foreach ($sfiles as $fname) {
+            $lname = substr($fname, -6, 2);
+            if (is_file($fname) && is_readable($fname)
+                && (!is_array($locales) || count($locales) == 0 || in_array($lname, $locales))
+            ) {
+                $ret[] = $lname;
             }
-            closedir($dh);
-            sort($ret);
         }
 
+        $mfiles = glob($dname . '??/??.php');
+        foreach ($mfiles as $fname) {
+            $lname = str_replace(array('/', '\\'), '_', substr($fname, -9, 5));
+            if (is_file($fname) && is_readable($fname)
+                && (!is_array($locales) || count($locales) == 0 || in_array($lname, $locales))
+            ) {
+                $ret[] = $lname;
+            }
+        }
+
+        sort($ret);
         return $ret;
     }
     // }}}
 
-    // {{{ raiseError()
     /**
-     * Trigger a PEAR error
+     * Load the given locale and return class name
      *
-     * To improve performances, the PEAR.php file is included dynamically.
+     * @param string $locale         Locale key, e.g. "de" or "en_US"
+     * @param string $requiredMethod Method that this class needs to have
      *
-     * @param string $msg error message
+     * @return string Locale class name
      *
-     * @return PEAR_Error
+     * @throws Numbers_Words_Exception When the class cannot be loaded
      */
-    function raiseError($msg)
+    public static function loadLocale($locale, $requiredMethod)
     {
-        return $msg;
-//        include_once 'PEAR.php';
-//        return PEAR::raiseError($msg);
+        $classname = 'Numbers_Words_Locale_' . $locale;
+        if (!class_exists($classname)) {
+            $file = str_replace('_', '/', $classname) . '.php';
+            if (stream_resolve_include_path($file)) {
+                include_once $file;
+            }
+
+            if (!class_exists($classname)) {
+                throw new Numbers_Words_Exception(
+                    'Unable to load locale class ' . $classname
+                );
+            }
+        }
+
+        $methods = get_class_methods($classname);
+
+        if (!in_array($requiredMethod, $methods)) {
+            throw new Numbers_Words_Exception(
+                "Unable to find method '$requiredMethod' in class '$classname'"
+            );
+        }
+
+        return $classname;
     }
-    // }}}
+
+    /**
+     * Removes redundant spaces, thousands separators, etc.
+     *
+     * @param string $num            Some number
+     * @param string $decimalPoint   The decimal mark, e.g. "." or ","
+     *
+     * @return string Number
+     */
+    function normalizeNumber($num, $decimalPoint = null)
+    {
+        if (is_null($decimalPoint)) {
+            $decimalPoint = $this->decimalPoint;
+        }
+
+        return preg_replace('/[^-'.preg_quote($decimalPoint).'0-9]/', '', $num);
+    }
 }
 
 // }}}
-?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# Numbers_Words
-convert
