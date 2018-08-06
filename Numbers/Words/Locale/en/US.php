d2c96c547ce88f16806159604fe748911377bd88
mamay@notan.ua <rfHfek12>
2018-08-06 14:04:35 +0300

    BDT fix1

--- Numbers/Words/Locale/en/US.php
+++ Numbers/Words/Locale/en/US.php
@@ -239,7 +239,6 @@ class Numbers_Words_Locale_en_US extends Numbers_Words
         'KWD' => array(array('Dinar'), array('fil')),
         'INR' => array(array('Rupee'), array('paise')),
         'BDT' => array(array('Taka','Taka'), array('poisha','poisha')),
-
         'LKR' => array(array('Rupee'), array('cent')),
         'TZS' => array(array('Tanzanian Shilling'), array('cent')),
 
