7a132dd9debdf1d95bf42a2613381d28b082783a
zagadum <zagadum@ukr.net>
2021-11-23 18:40:50 +0200

    fix not exist currency in Array

--- Numbers/Words/Locale/en/US.php
+++ Numbers/Words/Locale/en/US.php
@@ -506,8 +506,11 @@ class Numbers_Words_Locale_en_US extends Numbers_Words
         $int_curr = strtoupper($int_curr);
         if (!isset($this->_currency_names[$int_curr])) {
             $int_curr = $this->def_currency;
+            $curr_names =    array(array("$int_curr"), array('cent')); //По умолчанию если нет валюты
+        }else{
+            $curr_names = $this->_currency_names[$int_curr];
         }
-        $curr_names = $this->_currency_names[$int_curr];
+
 
         $ret = trim($this->_toWords($decimal));
         $lev = ($decimal == 1) ? 0 : 1;
